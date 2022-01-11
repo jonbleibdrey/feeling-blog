a013f67017a5d1de567366cdca1146970c083da5
jonathan bleibdrey <jonbleibdrey@aol.com>
2022-01-11 10:24:59 -0500

    added click events, functions and values

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 const Home = () => {
+
+  const handleClick = (e) => {
+    console.log("hello baby", e)
+  } 
+
+  const handleClickAgain = (name, e) =>{
+    console.log("hello" + name, e.target)
+
+  }
+
   return (
     <div className='home'>
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={(e)=>handleClickAgain(" mario", e)}>Click on me again</button>
       
     </div>
   )
