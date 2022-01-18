8c1fc7dccc198017e10a3d601177103a9333ccd8
jonathan bleibdrey <jonbleibdrey@aol.com>
2022-01-18 11:26:43 -0500

    added forms

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("jack");
+
   return (
     <div className="create">
       <h2>Add new blog</h2>
+      <form>
+        <label> Blog title:</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label> Blog body:</label>
+        <textarea
+          type="text"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label> Blog author:</label>
+        <select
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          required
+        >
+          <option value="henry"> Henry</option>
+          <option value="jack"> Jack</option>
+        </select>
+        <button>Add blog</button>
+      </form>
+      <h3>{title}</h3>
+      <p>{body}</p>
+      <p>By: {author}</p>
     </div>
   );
 };
