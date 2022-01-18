2d850e2ba6e3ced046941bb853465e13dbff6211
jonathan bleibdrey <jonbleibdrey@aol.com>
2022-01-18 11:48:52 -0500

    added delte function

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,14 +1,24 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetail = () => {
   const { id } = useParams();
+  const history = useHistory()
   const {
     data: blog,
     error,
     isPending,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  const handleClick= () => {
+    fetch(`http://localhost:8000/blogs/${id}`, {
+      method: "DELETE"
+    }).then(()=> {
+      history.push('/')
+    })
+    }
 
   return (
     <div className="blog-details">
@@ -20,6 +30,7 @@ const BlogDetail = () => {
           <p>Written by {blog.author}</p>
           <hr/>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
